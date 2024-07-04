5780e52a769f576a1194d2ebf5aebac09ce36e9d
Tushar Joshi <tusharjoshi@Pritis-MacBook-Pro.local>
2024-07-04 19:52:59 +0530

    created decorators

--- nject_socketio/test/socket_io_preconfig_test.ts
+++ nject_socketio/test/socket_io_preconfig_test.ts
@@ -14,6 +14,11 @@ import {
   SocketIOApplicationContext,
 } from "../core/socket_io_application_context";
 import { SocketIOApplication } from "../decorators/socket_application_decorator";
+import { SocketController } from "../decorators/socket_controller_decorator";
+import { SocketEvent } from "../decorators/socket_handler_decorator";
+import { RestController } from "../../nject_express/decorators/rest_controller_decorator";
+import { GET } from "../../nject_express/decorators/rest_handler_decorator";
+import { ResponseEntity } from "../../nject_express/core/server_entities/server_response_entity";
 
 const context = contextRegistry.registerContext(DEFAULT);
 const expessContext = new ExpressApplicationContext(context);
@@ -25,7 +30,30 @@ const socketContext = new SocketIOApplicationContext(
 contextRegistry.registerContext(EXPRESS_CONTEXT_NAME, expessContext);
 contextRegistry.registerContext(SOCKETIO_CONTEXT_NAME, socketContext);
 
-console.log(context.getAllObjects());
+@RestController("/test")
+class T {
+  @GET("/")
+  public a() {
+    return ResponseEntity.noContent();
+  }
+}
+
+console.log(context.getConstructorIDs());
+console.log(expessContext.getConstructorIDs());
+console.log(socketContext.getConstructorIDs());
+
+// TODO: fix why controllers not being stored in context
+@SocketController("sock")
+class Sock {
+  @SocketEvent("ping")
+  public event() {
+    console.log("ping");
+  }
+}
+
+console.log(context.getConstructorIDs());
+console.log(expessContext.getConstructorIDs());
+console.log(socketContext.getConstructorIDs());
 
 @ExpressApplication
 class ExpressApp implements ExpressApplicationContainer {}
